eb186514cbfeee35a705fe942453c8e0d2bbcbd0
Priyam Suthar <suthar.p@husky.neu.edu>
2020-09-28 15:41:47 -0400

    Setting up the environment

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require('./app');
+
+const db = require("./models");
+// db.sequelize.sync();
+
+db.sequelize.sync();
+
+// set port, listen for requests
+// const PORT = process.env.PORT || 8080;
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
