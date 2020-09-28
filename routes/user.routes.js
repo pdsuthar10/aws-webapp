d190c1382efab1d61214c07ead1571d4dc8a4a6e
Priyam Suthar <suthar.p@husky.neu.edu>
2020-09-28 15:46:51 -0400

    Setting up endpoints

---
+++ routes/user.routes.js
@@ -0,0 +1,17 @@
+module.exports = app => {
+    const users = require("../controllers/user.controller.js");
+
+    const router = require("express").Router();
+
+    router.post("/", users.create);
+
+    router.get("/checkAuthorization", users.isAuthorized);
+
+    router.post("/generateHash", users.generateHash);
+
+    router.get("/self", users.findOne);
+
+    router.put("/self", users.update);
+
+    app.use('/v1/users', router);
+};
