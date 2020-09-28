896a6030b3cbeb1177b643bc8ae64e7c6439256a
Priyam Suthar <suthar.p@husky.neu.edu>
2020-09-28 15:45:23 -0400

    Configuring database

---
+++ config/db.config.js
@@ -0,0 +1,16 @@
+// require('dotenv-flow').config();
+require('dotenv').config();
+
+module.exports = {
+    HOST: process.env.HOST,
+    USER: process.env.username,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DB,
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
