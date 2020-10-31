6036856c549ef257ca4059af6e6e8ee809d02d99
Priyam Suthar <suthar.p@husky.neu.edu>
2020-10-31 18:03:17 -0400

    Appspec.yml and GitFlow for a6 feature branch

---
+++ codedeploy/afterInstall.sh
@@ -0,0 +1,9 @@
+cd /home/ubuntu
+pwd
+sudo chown ubuntu: webapp/src
+sudo cp .env /home/ubuntu/webapp/src
+pwd
+cd /home/ubuntu/webapp/src
+
+npm install
+
---
+++ codedeploy/applicationStart.sh
@@ -0,0 +1,3 @@
+cd /home/ubuntu/webapp/src
+pwd
+pm2 start server.js
